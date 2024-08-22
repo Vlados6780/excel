717a1a58406227ca4c53081f889fb14227a96155
Vlados6780 <chuklinvlad8@gmail.com>
2024-08-22 12:41:19 +0300

    fixing bug with black edges

--- src/main/java/com/example/excel/service/DefaultGenerationExcelService.java
+++ src/main/java/com/example/excel/service/DefaultGenerationExcelService.java
@@ -38,6 +38,9 @@ public class DefaultGenerationExcelService implements GenerationExcelService {
             anchor.setRow1(0);
             anchor.setCol2(1);
             anchor.setRow2(1);
+            anchor.setDx1(-630000);
+            anchor.setDy1(-350000);
+
             XSSFPicture picture = drawing.createPicture(anchor, pictureIdx);
             picture.resize(1.01,1.0);
 
@@ -107,11 +110,11 @@ public class DefaultGenerationExcelService implements GenerationExcelService {
                 anchor2.setRow1(rowNum);
                 anchor2.setCol2(1);
                 anchor2.setRow2(++rowNum);
-                anchor2.setDx1(630000);
-                anchor2.setDy1(350000);
+                anchor2.setDx1(580000);
+                anchor2.setDy1(320000);
 
                 XSSFPicture picture2 = drawing2.createPicture(anchor2, pictureIdx2);
-                picture2.resize(0.74,0.93);
+                picture2.resize(0.87,0.93);
 
                 Cell cellProductPicture = row3.createCell(0);
                 CellStyle styleProductPicture = workbook.createCellStyle();
